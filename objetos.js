29cd3bfb2b25fede90669a6c4184affb6a7ea13f
Daniel Luengo <daniel.luengo.v@gmail.com>
2022-07-29 01:41:18 -0400

    Terminada la clase de objetos

--- objetos.js
+++ objetos.js
@@ -1,21 +1,57 @@
-var auto = {
-    marca : "chevrolet",
-    annio: 2022,
-    modelo: "ultimo modelo",
-    detalleDelAuto: function() {
-        console.log(`${auto.marca}, ${auto.annio}, ${auto.modelo}`)
-    }
-}
+// var auto = {
+//     marca : "chevrolet",
+//     annio: 2022,
+//     modelo: "ultimo modelo",
+//     detalleDelAuto: function() {
+//         console.log(`${auto.marca}, ${auto.annio}, ${auto.modelo}`)
+//     }
+// }
 
-auto.detalleDelAuto();
+// auto.detalleDelAuto();
 
 
-function autoNuevo (marca, annio, modelo) {
+// function autoNuevo (marca, annio, modelo) {
+//     this.marca = marca,
+//     this.annio = annio,
+//     this.modelo =modelo
+// }
+
+
+// var auto1 = new autoNuevo ("toyota", 2019, "Tercel");
+// console.log(auto1)
+
+
+
+
+
+
+function auto (marca, modelo, annio) {
     this.marca = marca,
-    this.annio = annio,
-    this.modelo =modelo
+    this.modelo = modelo,
+    this.annio = annio
 }
 
 
-var auto1 = new autoNuevo ("toyota", 2019, "Tercel");
-console.log(auto1)
+
+
+
+for (i=0; i<3; i++) {
+    
+    var laMarca = prompt("Ingrese marca");
+    var elModelo = prompt("Ingrese el modelo");
+    var ElAnnio = prompt("Ingrese el año")
+    var autonuevo = new auto (laMarca, elModelo, ElAnnio);
+    console.log(autonuevo);
+}
+
+
+
+
+
+
+
+
+
+
+
+
